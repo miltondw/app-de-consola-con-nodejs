2258f401015fc6e5bae8a288d16f8ac897685171
miltondw <estradamilton2001@gmail.com>
2021-04-07 19:23:18 -0500

    optimización del archivo yargs.js

--- config/yargs.js
+++ config/yargs.js
@@ -1,31 +1,20 @@
+const description={
+    require:true,
+    alias:'d',
+    desc:'Description of todo list'
+}
+const id={
+    alias:'i',
+    require:true  
+}
+const completed={
+    alias:'c',
+    default:true
+}
 const argv = require('yargs')
-            .command('create','create a tarea',{
-                description:{
-                    require:true,
-                    alias:'d',
-                    desc:'Description of todo list'
-                },
-                id:{
-                 alias:'i',
-                 require:true, 
-                }
-            })
-            .command('update','update a tarea',{
-                    id:{
-                        alias:'i',
-                        require:true  
-                    },
-                    completed:{
-                        alias:'c',
-                        default:true
-                    }
-            })
-            .command('delete','delete a tarea',{
-                id:{
-                    alias:'i',
-                    require:true  
-                }
-        })
+            .command('create','create a tarea',{id,description})
+            .command('update','update a tarea',{id,completed})
+            .command('delete','delete a tarea',{id})
             .help()
             .argv
 
